1784e595c35f9f291945a1abdbf8dc085bd14d78
Ziwei Lan <ziweilan92@gmail.com>
2020-11-26 11:53:35 +1100

    try fetch data from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dating-app-client';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      response => this.users = response,
+      error => console.log(error),
+    );
+  }
 }
