419008f54b407eb7db2f0f8c129eec4b7a0167c2
Ziwei Lan <ziweilan92@gmail.com>
2020-12-13 14:22:28 +1100

    1.move getUsers from app to home; 2. pass users to register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,24 +13,13 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
 
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService,
-  ) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'))
-  }
-
-  getUsers(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      response => this.users = response,
-      error => console.log(error),
-    );
   }
 }
