3fe59591803874877051673576a03875faac72e8
Ziwei Lan <ziweilan92@gmail.com>
2020-12-16 22:12:00 +1100

    remove redundant error logging

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -26,12 +26,8 @@ export class NavComponent implements OnInit {
 
   login() {
     this.accountService.login(this.model).subscribe(
-      response => {
+      () => {
         this.router.navigateByUrl('/members');
-      },
-      error => {
-        console.log(error);
-        this.toastr.error(error.error);
       }
     );
   }
